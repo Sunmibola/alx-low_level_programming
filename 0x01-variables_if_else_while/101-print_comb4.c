4edac1fcacade4d15f4d521ae861760dd30341fb
Sunmibola <olaniyisunmibola@gmail.com>
2022-09-13 14:13:43 -0700

    3 succession

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* three digits succession */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = '0'; x < '9'; x++)
+	{
+		for (x = y + 1; y <= '9'; y++)
+		{
+			for (z = y + 1; z <= '9'; z++)
+			{
+				if (x != y && x != z && y != z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+
+					if (x == '6' && y == '7' && z == '8')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
