c719e706f14bb5a84e935d0efb0558ecf6cfb8b6
Sunmibola <olaniyisunmibola@gmail.com>
2022-09-14 02:54:22 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of digits
+ *@n: assigned to serve as a checker
+ * Return: if statements
+ */
+
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
+
+	return (value);
+}
