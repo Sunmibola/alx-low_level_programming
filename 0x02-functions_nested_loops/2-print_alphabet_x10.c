570e34f499aff56222ba639dfdbb92dff0c03d92
Sunmibola <olaniyisunmibola@gmail.com>
2022-09-13 23:41:41 -0700

    10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+	}
+	_putchar('\n');
+	j++;
+}
