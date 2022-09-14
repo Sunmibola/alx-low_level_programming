972df2b4b14014680c209b3a6671377095ead885
Sunmibola <olaniyisunmibola@gmail.com>
2022-09-14 04:28:21 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ *@n: integer value to be tested
+ *
+ * Return: Always 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
