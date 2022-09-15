54ebec7a0d174db62049ef8f3ea499ebe1e4dd71
Sunmibola <olaniyisunmibola@gmail.com>
2022-09-15 15:22:58 -0700

    first 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print first 98  fibonacci numbers
+ * then next line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+	unsigned long n1 = 0, n2 = 1, n3;
+	unsigned long h1, h2;
+	int i;
+
+	for (i = 0; i <= 92; i++)
+	{
+		n3 = n1 + n2;
+		printf("%lu, ", n3);
+		n1 = n2;
+		n2 = n3;
+	}
+	n1_h1 = n1 / 10000000000;
+	n2_h1 = n2 / 10000000000;
+	n1_h2 = n1 % 10000000000;
+	n2_h2 = n2 % 10000000000;
+	for (i = 93;  i < 99; i++)
+	{
+		h1 = n1_h1 + n2_h1;
+		h2 = n1_h2 + n2_h2;
+		if ((n1_h2 + n2_h2) > 9999999999)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
+		printf("%lu%lu", h1, h2);
+		if (i != 98)
+			printf(", ");
+
+		n1_h1 = n2_h1;
+		n1_h2 = n2_h2;
+		n2_h1 = h1;
+		n2_h2 = h2;
+
+	}
+	printf("\n");
+
+	return (0);
+
+}
