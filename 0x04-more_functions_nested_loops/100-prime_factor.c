72ac0edc782bdf51a0194cb1891175a31d052bfa
Sunmibola <olaniyisunmibola@gmail.com>
2022-09-19 12:22:28 -0700

    print prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+
+/**
+ * main - Entry point
+ * prints prime factor
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%ld\n", maxf);
+
+	return (0);
+}
