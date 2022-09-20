ff77e03c0b94e159d385713d392776d4381c394c
Sunmibola <olaniyisunmibola@gmail.com>
2022-09-20 10:41:20 -0700

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+# include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string
+ * Return: void
+ */
+void puts_half(char *str)
+
+{
+	int n, length, mid;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		mid = length / 2;
+		for (n = mid; n < length; n++)
+		{
+			_putchar(str[n]);
+		}
+
+	}
+	else
+	{
+		mid = (length - 1) / 2;
+		for (n = mid + 1; n < length; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar('\n');
+}
