2fb2face6ec578e67c6d2d0049ecce06b33982de
Sunmibola <olaniyisunmibola@gmail.com>
2022-09-20 11:06:10 -0700

    printfunc

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+# include "main.h"
+# include <stdio.h>
+
+/**
+ * print_array - print n elements of an array
+ * @a: pointer to array
+ * @n: number of array
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ void print_rev(char *s);
 void puts_half(char *str);
 void puts_half(char *str);
 void puts2(char *str);
+void print_array(int *a, int n);
