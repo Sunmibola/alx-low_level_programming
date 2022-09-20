9e370042d9389054c9f8efaf1000d246e13b54ea
Sunmibola <olaniyisunmibola@gmail.com>
2022-09-20 03:41:09 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,34 @@
+# include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: character s to be printed
+ * Return: return void
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i, c, k;
+	char *a, aux;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
+}
