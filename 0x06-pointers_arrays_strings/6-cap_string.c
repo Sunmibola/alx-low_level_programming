fc14e28af1e926198a512432d47e58d3f1e64d6e
Sunmibola <olaniyisunmibola@gmail.com>
2022-09-22 06:46:04 -0700

    capitalize word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+# include "main.h"
+
+/**
+ * cap_string - capitalizes each word
+ * @s: pointer
+ * Return: to *s
+ */
+
+char *cap_string(char *s)
+{
+	int a = 0, i;
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[a])
+	{
+		i = 0;
+
+		while (i < cspc)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+			i++;
+		}
+		a++;
+	}
+
+	return (s);
+}
