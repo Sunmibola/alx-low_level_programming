b26f4e2ce72031ae84c9c63c9577e19d0ea32538
Sunmibola <olaniyisunmibola@gmail.com>
2022-10-05 09:43:59 -0700

    currency change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- min coins for change
+ * @argc: argument count
+ * @argv: argument
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int total, count;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	total = strtol(argv[1], &p, 10);
+	count = 0;
+
+	if (!*p)
+	{
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (total >= cents[i])
+				{
+					count += total / cents[i];
+					total = total % cents[i];
+				}
+			}
+		}
+		if (total == 1)
+			count++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", count);
+	return (0);
+}
