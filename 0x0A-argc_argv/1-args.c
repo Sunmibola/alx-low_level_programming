1b77de22e2f75307c992ae1408e443c3e725aa7b
Sunmibola <olaniyisunmibola@gmail.com>
2022-10-05 09:06:56 -0700

    arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - arguments
+ * @argc: argument count
+ * @argv: argument
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
